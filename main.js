c3e004e62364bc3312ae986a157f1c94e479d9e1
VincentMugondora <vinmugondora@gmail.com>
2024-05-03 12:20:05 +0200

    intial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const darkMode = document.querySelector('.dark')
+const selectTheme = document.querySelector('.theme')
+const select = document.querySelector('.theme')
+const select2 = document.querySelector('.theme2')
+
+selectTheme.addEventListener('click', function(event) {
+    darkMode.classList.toggle('black')
+    select.classList.toggle('witch')
+    // console.log('clicked');
+    // alert(event.target)
+})
+
+select2.addEventListener('click', function(event) {
+    darkMode.classList.toggle('black')
+    select.classList.toggle('witch')
+    // console.log('clicked');
+    // alert(event.target)
+})
+
+const menu = document.querySelector('.hamburger')
+const menuOne = document.querySelector('.overlay')
+
+menu.addEventListener('click', function(){
+    menuOne.classList.toggle('vincent')
+    menu.classList.toggle('active-lines')
+})
